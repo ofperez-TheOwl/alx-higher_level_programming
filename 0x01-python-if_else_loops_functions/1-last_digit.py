13936576d4fc0cbab51adb52d839ab3157b38587
Elfueros <odidiperez@gmail.com>
2022-06-02 01:45:03 +0000

    all tasks except C tasks

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+if (number < 0):
+    number = -1 * number
+print(f"Last digit of {number} ", end='')
+number = number % 10
+print(f"is {number} ", end='')
+if (number == 0):
+    print("and is zero")
+elif (number > 5):
+    print("and is greater than 5")
+else :
+    print("and is less than 6 and not 0")
---
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+for i in range(0, 99):
+    print(f"{i}", end=', ')
+print(f"{i + 1}")
