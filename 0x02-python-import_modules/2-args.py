403c7968864a5a771d31c5276c2186bcf07549b0
Elfueros <odidiperez@gmail.com>
2022-06-02 23:17:07 +0000

    task 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import sys
+arg = sys.argv
+if (len(arg) == 1):
+    print("0 arguments.")
+else:
+    if (len(arg) == 2):
+        print("1 argument:")
+        print("1: {:s}".format(arg[1]))
+    else:
+        print("{:d} arguments:".format(len(arg) - 1))
+        for i in range(1, len(arg)):
+            print("{:d}: {:s}".format(i, arg[i]))
