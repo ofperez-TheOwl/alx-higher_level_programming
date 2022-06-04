51b67e19e45ef9787de0df1fa870cc58038f160d
Elfueros <odidiperez@gmail.com>
2022-06-04 19:44:20 +0000

    task 0

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def print_list_integer(my_list=[]):
+    if (len(my_list) != 0):
+        for i in my_list:
+            print("{:d}".format(i))
+
+print_list_integer([1, 2, 3, 4])
