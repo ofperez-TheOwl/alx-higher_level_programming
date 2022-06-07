a965d5d6d52a300180f71a49ddca644a75cd8061
Elfueros <odidiperez@gmail.com>
2022-06-07 07:28:50 +0000

    task 14 review

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,26 +1,35 @@
-#include "Python.h"
+#include <Python.h>
+#define Py_TYPE(o) (((PyObject*)(o))->ob_type)
 
-/*
-includes listobject.h
-VIEW HEADER-> https://github.com/python/cpython/blob/master/Include/listobject.h
-VIEW MANUAL-> https://docs.python.org/3.4/c-api/list.html
-includes object.h
-VIEW HEADER-> https://docs.python.org/3.4/c-api/structures.html)
-VIEW MANUAL-> https://github.com/python/cpython/blob/master/Include/object.h
-*/
-
+/**
+ * print_python_list_info - printbasic info about Python lists
+ * @p: pointer to PyObject; the list object
+ *
+ * Return: nothing
+ */
 void print_python_list_info(PyObject *p)
 {
-	Py_ssize_t size, alloc, idx;
+	Py_ssize_t i = 0;
+	Py_ssize_t j, k;
+	PyObject *tmp;
 
-	size = PyList_Size(p);
-	alloc = ((PyListObject *)p)->allocated;
-	printf("[*] Size of the Python List = %ld\n", size);
-	printf("[*] Allocated = %ld\n", alloc);
-	for (idx = 0; idx < size; idx++)
+	if (PyList_CheckExact(p) != 0) /* check if p is a list */
 	{
-		printf("Element %ld: %s\n",
-		       idx,
-		       (PY_TYPE(PyList_GetItem(p, idx)))->tp_name);
+		/* find and print number of element in p */
+		j = PyList_Size(p);
+		k = ((PyListObject *)p)->allocated;
+		printf("[*] Size of the Python List = %ld\n", j);
+		printf("[*] Allocated  = %ld\n", k);
+		if (j == 0)
+			printf("Element 0: 0\n");
+		else
+		/* print the type of each element of the p */
+			while (i < j)
+			{
+			/* find type of element at index i */
+				tmp = PyList_GetItem(p, i);
+				printf("Element %ld: %s\n", i, ((Py_TYPE(tmp))->tp_name));
+				i++;
+			}
 	}
 }
