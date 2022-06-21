e67d89f410d207d4c5f8afacaa3b19f42da63a83
Elfueros <odidiperez@gmail.com>
2022-06-21 01:26:12 +0000

    mandatory tasks completed

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    i , j = 0, 0
+    result = []
+    while i < list_length:
+        try:
+            j = my_list_1[i] / my_list_2[i]
+            result.append(j)
+        except TypeError:
+            print("wrong type")
+            result.append(0)
+        except ZeroDivisionError:
+            print("division by 0")
+            result.append(0)
+        except IndexError:
+            print("out of range")
+            result.append(0)
+        finally:
+            i = i + 1
+    return (result)
--- 0x05-python-exceptions/main.py
+++ 0x05-python-exceptions/main.py
@@ -1,15 +1,7 @@
 #!/usr/bin/python3
-safe_print_list_integers = \
-    __import__('2-safe_print_list_integers').safe_print_list_integers
+raise_exception_msg = __import__('6-raise_exception_msg').raise_exception_msg
 
-my_list = [1, 2, 3, 4, 5]
-
-nb_print = safe_print_list_integers(my_list, 2)
-print("nb_print: {:d}".format(nb_print))
-
-my_list = [1, 2, 3, "School", 4, 5, [1, 2, 3]]
-nb_print = safe_print_list_integers(my_list, len(my_list))
-print("nb_print: {:d}".format(nb_print))
-
-nb_print = safe_print_list_integers(my_list, len(my_list) + 2)
-print("nb_print: {:d}".format(nb_print))
+try:
+    raise_exception_msg("C is fun")
+except NameError as ne:
+    print(ne)
