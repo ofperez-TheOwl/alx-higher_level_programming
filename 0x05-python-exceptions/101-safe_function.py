7d543aebc2a02062b7781ec2dd1bd10abca6ea47
Elfueros <odidiperez@gmail.com>
2022-06-21 09:54:39 +0000

    review of task 8

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -7,6 +7,6 @@ def safe_function(fct, *args):
     try:
         i = fct(args[0], args[1])
         return (i)
-    except (IndexError, TypeError, ValueError, ZeroDivisionError) as e:
+    except (IndexError, TypeError, ValueError, ZeroDivisionError, NameError) as e:
         print("Exception: {}".format(e), file=sys.stderr)
         return (i)
