38ea30d6a8001ca7e9b981eeacde1fdac255ed0f
Elfueros <odidiperez@gmail.com>
2022-06-21 09:40:35 +0000

    task 7 to 9

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import sys
+def safe_function(fct, *args):
+    i = None
+    try:
+        i = fct(args[0], args[1])
+        return (i)
+    except (IndexError, TypeError, ValueError, ZeroDivisionError) as e:
+        print("Exception: {}".format(e), file=sys.stderr)
+        return (i)
--- 0x05-python-exceptions/6-raise_exception_msg.py
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 def raise_exception_msg(message=""):
+    print(NameError)
     raise NameError("C is fun")
