4ab58a7d02bfd4250474c1c5c30f99086def0acc
Elfueros <odidiperez@gmail.com>
2022-06-25 23:46:36 +0000

    reviews

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,4 +1,4 @@
-#!/usr/bin/pyhton3
+#!/usr/bin/python3
 """Module3-common_elements
 """
 
--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -5,5 +5,5 @@
 
 def number_keys(a_dictionary):
     if (a_dictionary is not None):
-        return (len(a_dictionnary.keys()))
+        return (len(a_dictionary.keys()))
     return (0)
