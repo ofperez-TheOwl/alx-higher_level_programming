a4f02c4af0a05b60b976edd4c21bcb8bf68b7514
Elfueros <odidiperez@gmail.com>
2022-06-26 18:41:53 +0000

    advanced tasks

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -8,7 +8,7 @@ def roman_to_int(roman_string):
     uniq = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     substr = {'IV': 4, 'IX': 9, 'XL': 40, 'XC': 90, 'CD': 400, 'CM': 900}
     i, result = 0, 0
-    if roman_string is None or len(roman_string) == 0:
+    if roman_string is None or roman_string == "":
         return (0)
     while (i < len(roman_string)):
         if roman_string[i:i + 2] in substr:
