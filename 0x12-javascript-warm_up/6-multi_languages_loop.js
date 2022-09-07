9a5a452579c0f246a92d577df59225947437117a
ofperez-TheOwl <odidiperez@gmail.com>
2022-09-07 23:28:38 +0000

    advanced tasks

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-const line = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-for (const sentence of line) {
-  console.log(sentence);
+const lines = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+for (const word of lines) {
+  console.log(word);
 }
