d87394adcc60bd26223b9b27db8de86172725cb2
ofperez-TheOwl <odidiperez@gmail.com>
2022-09-11 17:16:11 +0000

    Tasks 0 to 3

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Module : 1-filter_states
+lists all states from the given database which names start by "N"
+Usage: .:/0-select_states <username> <password> <database_name>
+"""
+
+
+from sys import argv
+import MySQLdb as _db
+
+if __name__ == "__main__":
+    username, password, database_name = argv[1], argv[2], argv[3]
+    dtbase = _db.connect(host="localhost", port=3306, user=username,
+                         passwd=password, db=database_name)
+    pointer = dtbase.cursor()
+    pointer.execute("SELECT * FROM states\
+                     WHERE name LIKE 'N%' \
+                     ORDER BY id ASC")
+    table_rows = pointer.fetchall()
+    for row in table_rows:
+        print(row)
+    pointer.close()
+    dtbase.close()
---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Module : 2-my_safe_filter_states
+lists all states from the given database which names respect given patterns
+Usage: .:/0-select_states <username> <password> <database_name> <pattern>
+"""
+
+
+from sys import argv
+import MySQLdb as _db
+
+if __name__ == "__main__":
+    username, password, database_name = argv[1], argv[2], argv[3]
+    dtbase = _db.connect(host="localhost", port=3306, user=username,
+                         passwd=password, db=database_name)
+    pointer = dtbase.cursor()
+    pointer.execute("SELECT * FROM states\
+                     WHERE name LIKE %s\
+                     ORDER BY id ASC", (argv[4],))
+    table_rows = pointer.fetchall()
+    for row in table_rows:
+        print(row)
+    pointer.close()
+    dtbase.close()
